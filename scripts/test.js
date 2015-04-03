b5ca84370883168a90f7cc3278f86f249e50a8c1
Molly Kent <kent.molly@gmail.com>
2015-04-03 13:48:38 -0700

    make functioning test of function call and add content

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,18 @@
 (function() {
-	my
+
+	Element.prototype.prependChild = function(child) { //Googled to find this - sweet!
+		this.insertBefore(child, this.firstChild); 
+	};
+
+	var elOne = document.getElementById('round1-guess');
+	elOne.onclick = doIt;
+
+	function doIt() {	
+		var newSection = document.createElement('section');
+		newSection.textContent = 'you did it!'
+		var position = document.getElementsByTagName('main')[0];
+		position.prependChild(newSection);
+
+}
+
 })();
